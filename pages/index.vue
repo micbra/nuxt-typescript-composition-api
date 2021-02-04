29c177573cea3a6b9cbf4060f2770f7207115448
Michael Braunegger <michael.braunegger@deptagency.com>
2021-02-04 10:12:54 +0100

    install nuxt with typescript and composition-api

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello World!
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup () {
+    const api = 'https://randomuser.me/api/?results=10&inc=name,dob,picture'
+  }
+})
+</script>
