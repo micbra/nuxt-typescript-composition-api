3abd1352b48b53224f68866df99f0bd7d07dbfb4
Michael Braunegger <michael.braunegger@deptagency.com>
2021-02-04 14:03:13 +0100

    prepare examples with useFetch and useAsync

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, useAsync, useFetch } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup () {
-    const api = 'https://randomuser.me/api/?results=10&inc=name,dob,picture'
+
+    // first uncomment the line with useFetch() and notice the response time in the devtools network tab, then compare with the useAsync() line
+
+    // useFetch(() => console.log('response time >200ms'))
+
+    // useAsync(() => console.log('response time ~15ms'))
   }
 })
 </script>
